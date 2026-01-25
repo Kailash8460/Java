b912589fd79a5d2dede79ebf2f14a4cbcbb63855
Kailash8460 <kailashbhagchandani60@gmail.com>
2026-01-25 23:53:40 +0530

    Palindrome Triangular Pattern Code

---
+++ Patterns/PalindromeTriangular.java
@@ -0,0 +1,36 @@
+package Patterns;
+
+import java.util.*;
+
+public class PalindromeTriangular {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        // int rows = 4;
+        printPattern(rows);
+        sc.close();
+    }
+
+    public static void printPattern(int rows) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = rows - i; j >= 1; j--) {
+                System.out.print(" " + " ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            for (int j = 1; j <= i; j++) {
+                if (i == 1) {
+                    System.out.print(" ");
+                    break;
+                }
+                if (j == 1) {
+                    continue;
+                }
+                System.out.print(j + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
