958020f56c33ab4b747c9a98483b157d67b2837f
Kailash8460 <kailashbhagchandani60@gmail.com>
2026-01-28 08:44:20 +0530

    Reverse String achieveing Time Complexity

--- String/ReverseString.java
+++ String/ReverseString.java
@@ -8,15 +8,29 @@ public class ReverseString {
         System.out.println("Enter a String to reverse: ");
         String input = sc.nextLine();
         String result = reverseString(input);
+        System.out.println("Using Reverse String.");
         System.out.println(result);
+        StringBuilder resultStringBuilder = reverseUsingStringBuilder(input);
+        System.out.println("Using Reverse StringBuilder.");
+        System.out.println(resultStringBuilder);
         sc.close();
     }
 
+    // Time Complexity: O(n) not achieved
     public static String reverseString(String input) {
         String result = "";
         for (int i = 0; i < input.length(); i++) {
             char ch = input.charAt(i);
             result = ch + result;
+        }
+        return result;
+    }
+
+    // Time Complexity: O(n) achieved
+    public static StringBuilder reverseUsingStringBuilder(String input) {
+        StringBuilder result = new StringBuilder();
+        for (int i = input.length() - 1; i >= 0; i--) {
+            result.append(input.charAt(i));
         }
         return result;
     }
