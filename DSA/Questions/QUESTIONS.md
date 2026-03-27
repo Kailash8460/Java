37bcb6c41ff4a3900aaa0c1fd2f8a59f0bbbb178
Kailash8460 <kailashbhagchandani60@gmail.com>
2026-03-27 18:23:56 +0530

    DSA questions - Solution 1

---
+++ DSA/Questions/QUESTIONS.md
@@ -0,0 +1,22 @@
+# Data Structure and Algorithms
+
+## Question 1:
+
+Given an array of integers, find the largest element.
+
+### Samples:
+
+```text
+Input 1: [3, 7, 2, 9, 5]
+Output: 9
+
+Input 2: [1, 1, 1, 1]
+Output: 1
+
+Input 3: [-5, -2, -9, -1]
+Output: -1
+```
+
+### Solution: 
+
+Go to the **Solution_01.java** file.
---
+++ DSA/Solutions/Solution_01.java
@@ -0,0 +1,17 @@
+package DSA.Solutions;
+
+public class Solution_01 {
+    public static void main(String[] args) {
+        int[] arr = {3, 7, 2, 9, 5};
+        // int[] arr = {1, 1, 1, 1};
+        // int[] arr = {-5, -2, -9, -1};
+        int length = arr.length;
+        int max = arr[0];
+        for (int i = 0; i < length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("The largest element in the array is: " + max);
+    }
+}
